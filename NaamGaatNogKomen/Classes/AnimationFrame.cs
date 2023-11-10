eab49cbda55caed0bb844df1960d37d76bd15e00
Tyler <Maximiliandudajunior@gmail.com>
2023-11-10 20:53:42 +0100

    Add sourceRectangle to AnimationFrame class

--- NaamGaatNogKomen/Classes/AnimationFrame.cs
+++ NaamGaatNogKomen/Classes/AnimationFrame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace NaamGaatNogKomen.Classes
 {
     internal class AnimationFrame
     {
+        public Rectangle SourceRectangle { get; set; }
+
+        public AnimationFrame(Rectangle sourceRectangle)
+        {
+            SourceRectangle = sourceRectangle;
+        }
     }
 }
