ddf16beda01cf69fedfb23f1dbd803ed7fdcdf0d
Tyler <Maximiliandudajunior@gmail.com>
2023-11-10 22:05:54 +0100

    Add variables to Game1 class

--- NaamGaatNogKomen/Game1.cs
+++ NaamGaatNogKomen/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using NaamGaatNogKomen.Classes;
 
 namespace NaamGaatNogKomen
 {
@@ -8,6 +9,8 @@ namespace NaamGaatNogKomen
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _heroTexture;
+        private Hero hero;
 
         public Game1()
         {
