35fb4b6a0e63e721eaa530a43be159ec2ece36a1
Haitam Baqloul <Haitam.baqloul@hotmail.com>
2023-11-12 13:01:38 +0100

    Add 2 variables: texture en vector

--- NaamGaatNogKomen/Classes/TilesSet/Tiles.cs
+++ NaamGaatNogKomen/Classes/TilesSet/Tiles.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,7 @@ namespace NaamGaatNogKomen.Classes.TilesSet
 {
     internal class Tiles
     {
-
+        protected Texture2D texture;
+        protected Vector2 position;
     }
 }
