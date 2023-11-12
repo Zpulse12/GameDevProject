4b24347dcc3a1b37a47599de8b9cd6a83669f0b6
Tyler <Maximiliandudajunior@gmail.com>
2023-11-12 02:55:10 +0100

    Paste code from Update method in Hero class to ReadInput method in KeybaordReader class

--- NaamGaatNogKomen/Classes/Input/KeyboardReader.cs
+++ NaamGaatNogKomen/Classes/Input/KeyboardReader.cs
@@ -12,7 +12,13 @@ namespace NaamGaatNogKomen.Classes.Input
     {
         public Vector2 ReadInput()
         {
-
+            KeyboardState state = Keyboard.GetState();
+            Vector2 direction = Vector2.Zero;
+            if (state.IsKeyDown(Keys.Left))
+                direction.X -= 1;
+            if (state.IsKeyDown(Keys.Right))
+                direction.X += 1;
+            return direction;
         }
     }
 }
