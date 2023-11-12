e87af0c7d820c55cec41b4e13862fe5b79aa8f48
Haitam Baqloul <Haitam.baqloul@hotmail.com>
2023-11-12 20:24:08 +0100

    Add method to tiles

--- NaamGaatNogKomen/Classes/TilesSet/Tiles.cs
+++ NaamGaatNogKomen/Classes/TilesSet/Tiles.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +12,16 @@ namespace NaamGaatNogKomen.Classes.TilesSet
     internal class Tiles
     {
         protected Texture2D texture;
-        protected Vector2 position;
+        protected Vector2 position; // gaat de posititie bepalen van de texture
 
         public Tiles(Texture2D texture, Vector2 position)
         {
             this.texture = texture;
             this.position = position;
+        }
+        public virtual void Draw(SpriteBatch sprite)
+        {
+            sprite.Draw(texture, position, Color.White);//Color.White lets the sprite be drawen with the original texture
         }
     }
 }
