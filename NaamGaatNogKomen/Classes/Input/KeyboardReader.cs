e984445abe7a68846bca65d4ea476a6aa9004bac
Tyler <Maximiliandudajunior@gmail.com>
2023-11-12 02:53:55 +0100

    Add ReadInput method to KeyboardReader class

--- NaamGaatNogKomen/Classes/Input/KeyboardReader.cs
+++ NaamGaatNogKomen/Classes/Input/KeyboardReader.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,9 @@ namespace NaamGaatNogKomen.Classes.Input
 {
     internal class KeyboardReader
     {
+        public Vector2 ReadInput()
+        {
+
+        }
     }
 }
