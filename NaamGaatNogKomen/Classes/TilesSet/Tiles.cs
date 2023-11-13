7b3657d3e55c1466d2c9b088435625ecde13dd92
Haitam <Haitam.baqloul@hotmail.com>
2023-11-13 18:51:51 +0100

    Fix tiles

---
+++ NaamGaatNogKomen/Classes/TilesSet/Tiles.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaamGaatNogKomen.Classes.TilesSet
+{
+    internal class Tiles
+    {
+        protected Texture2D texture;
+        protected Vector2 position;
+
+        public Tiles(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+        public virtual void Draw(SpriteBatch spriteBatch) 
+        {
+            spriteBatch.Draw(texture, position, Color.White);//Color.white let's tiles
+        }
+    }
+}
