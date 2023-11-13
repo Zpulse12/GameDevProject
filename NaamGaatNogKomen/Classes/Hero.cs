2ad920636dc8df44919a70334b2293961de6b0ff
Tyler <Maximiliandudajunior@gmail.com>
2023-11-13 14:57:08 +0100

    Add idleANimation variable

--- NaamGaatNogKomen/Classes/Hero.cs
+++ NaamGaatNogKomen/Classes/Hero.cs
@@ -16,6 +16,7 @@ namespace NaamGaatNogKomen.Classes
     {
         private Texture2D texture;
         private Animation walkingAnimation;
+        private Animation idleAnimation;
         private Vector2 position = new Vector2(0, 0);
         private Vector2 speed = new Vector2(0, 0);
         private Vector2 acceleration = new Vector2(0.001f, 1f);
