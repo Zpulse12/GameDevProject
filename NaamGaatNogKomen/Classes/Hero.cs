a78c08c72bb33698459945a805923029abf59f54
Tyler <Maximiliandudajunior@gmail.com>
2023-11-25 19:02:42 +0100

    Delete code that was wrong

--- NaamGaatNogKomen/Classes/Hero.cs
+++ NaamGaatNogKomen/Classes/Hero.cs
@@ -54,10 +54,8 @@ namespace NaamGaatNogKomen.Classes
                 spriteBatch.Draw(walkTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White,0, new Vector2(), 3, SpriteEffects.None, 0);
             else if (isMoving == true && inputReader.ReadInput().X == -1)
                 spriteBatch.Draw(walkTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White, 0, new Vector2(), 3, SpriteEffects.FlipHorizontally,0);
-            else if (inputReader.ReadInput().X == 1)
+            else
                 spriteBatch.Draw(idleTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White,0, new Vector2(), 3, SpriteEffects.None, 0);
-            else if (inputReader.ReadInput().X == -1)
-                spriteBatch.Draw(idleTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White, 0, new Vector2(), 3, SpriteEffects.FlipHorizontally, 0);
         }
         public void Update(GameTime gameTime)
         {
