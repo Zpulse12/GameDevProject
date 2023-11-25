3b9460b5ecee12722b55cca2a5b44a0ab489ac6d
Tyler <Maximiliandudajunior@gmail.com>
2023-11-25 18:58:40 +0100

    Add else if because else didnt work

--- NaamGaatNogKomen/Classes/Hero.cs
+++ NaamGaatNogKomen/Classes/Hero.cs
@@ -56,7 +56,7 @@ namespace NaamGaatNogKomen.Classes
                 spriteBatch.Draw(walkTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White, 0, new Vector2(), 3, SpriteEffects.FlipHorizontally,0);
             else if (inputReader.ReadInput().X == 1)
                 spriteBatch.Draw(idleTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White,0, new Vector2(), 3, SpriteEffects.None, 0);
-            else
+            else if (inputReader.ReadInput().X == -1)
                 spriteBatch.Draw(idleTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White, 0, new Vector2(), 3, SpriteEffects.FlipHorizontally, 0);
         }
         public void Update(GameTime gameTime)
