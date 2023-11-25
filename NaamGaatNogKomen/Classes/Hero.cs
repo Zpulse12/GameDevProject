e12f319cbe9ea577fc08041949d743e62ca63c5d
Tyler <Maximiliandudajunior@gmail.com>
2023-11-25 19:12:00 +0100

    Add lastMovementDirection to Hero class

--- NaamGaatNogKomen/Classes/Hero.cs
+++ NaamGaatNogKomen/Classes/Hero.cs
@@ -29,6 +29,7 @@ namespace NaamGaatNogKomen.Classes
         private IInputReader inputReader;
         private int _screenWidth; 
         private int _screenHeight;
+        private int lastMovementDirection = 1;
 
         public Hero(Texture2D walkTexture, Texture2D idleTexture, IInputReader inputReader)
         {
@@ -55,7 +56,7 @@ namespace NaamGaatNogKomen.Classes
             else if (isMoving == true && inputReader.ReadInput().X == -1)
                 spriteBatch.Draw(walkTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White, 0, new Vector2(), 3, SpriteEffects.FlipHorizontally,0);
             else
-                spriteBatch.Draw(idleTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White,0, new Vector2(), 3, SpriteEffects.None, 0);
+                spriteBatch.Draw(idleTexture, position, currentAnimation.CurrentFrame.SourceRectangle, Color.White, 0, new Vector2(), 3, SpriteEffects.None, 0);
         }
         public void Update(GameTime gameTime)
         {
