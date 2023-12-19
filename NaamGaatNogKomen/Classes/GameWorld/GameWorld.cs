f5d0605cb8345d1fb060c080e5a25764c3b5c429
Haitam <Haitam.baqloul@hotmail.com>
2023-12-19 20:54:10 +0100

    add new class and add variables to it

---
+++ NaamGaatNogKomen/Classes/GameWorld/GameWorld.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using NaamGaatNogKomen.Classes.TilesSet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaamGaatNogKomen.Classes.GameWorld
+{
+    internal class GameWorld
+    {
+        private FloorTiles floorTiles;
+        private Hero hero;
+    }
+}
