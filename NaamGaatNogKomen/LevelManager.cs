ba9c8924240ca6f1af012d74d70b5cfe30cb6c39
Haitam <Haitam.baqloul@hotmail.com>
2024-06-27 10:33:09 +0200

    add a levelmanager

---
+++ NaamGaatNogKomen/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace NaamGaatNogKomen
+{
+    public class LevelManager
+    {
+        private List<Level> levels;
+        private int currentLevelIndex;
+
+        public LevelManager(List<Level> levels)
+        {
+            this.levels = levels;
+            currentLevelIndex = 0;
+        }
+
+        public Level CurrentLevel => levels[currentLevelIndex]; // Eigenschap om de huidige level te krijgen
+
+        public void NextLevel()
+        {
+            if (currentLevelIndex < levels.Count - 1)
+            {
+                currentLevelIndex++;
+            }
+            else
+            {
+                // Handle end of game or loop back to first level
+                currentLevelIndex = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            CurrentLevel.Draw(spriteBatch);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            CurrentLevel.Update(gameTime);
+        }
+    }
+}
