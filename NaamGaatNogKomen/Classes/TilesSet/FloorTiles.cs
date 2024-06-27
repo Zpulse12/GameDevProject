98465e3e433d15463621f44aa62b0fc9665ba800
Haitam <Haitam.baqloul@hotmail.com>
2024-06-27 10:32:23 +0200

    change how floor spawn in

--- NaamGaatNogKomen/Classes/TilesSet/FloorTiles.cs
+++ NaamGaatNogKomen/Classes/TilesSet/FloorTiles.cs
@@ -1,37 +1,55 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NaamGaatNogKomen.Classes.TilesSet
 {
     public class FloorTiles
     {
         private Texture2D floors;
-        private Animation floorAnimation = new Animation();
+        private int numberOfColumns; // Number of tiles in the width of the tileset
+        private int tileWidth; // Width of each tile
+        private int tileHeight; // Height of each tile
+        private List<Rectangle> tiles;
 
-        public FloorTiles(Texture2D floor)
+        public FloorTiles(Texture2D floor, int numberOfColumns)
         {
             this.floors = floor;
-            floorAnimation.GetFramesFromTexture(floor.Width, floor.Height, 4, 1);
+            this.numberOfColumns = numberOfColumns;
+            this.tileWidth = floors.Width / numberOfColumns;
+            this.tileHeight = floors.Height / numberOfColumns;
+            this.tiles = new List<Rectangle>();
         }
 
-        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        public void GenerateTiles(int screenWidth)
         {
-            AnimationFrame currentFrame = floorAnimation.CurrentFrame;
-            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, GraphicsDeviceManager.DefaultBackBufferWidth, floors.Height);
-            spriteBatch.Draw(floors, destinationRectangle, currentFrame.SourceRectangle, Color.White);
+            tiles.Clear();
+            // Generate bottom row
+            for (int x = 0; x < screenWidth; x += tileWidth)
+            {
+                tiles.Add(new Rectangle(x, 600 - tileHeight, tileWidth, tileHeight));
+            }
         }
 
-        public void Update(GameTime gameTime)
+        public List<Rectangle> GetWalkableTiles()
         {
-            floorAnimation.Update(gameTime);
+            return tiles;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(0, (numberOfColumns - 1) * tileHeight, tileWidth, tileHeight);
+            foreach (var tile in tiles)
+            {
+                spriteBatch.Draw(floors, tile, sourceRectangle, Color.White);
+            }
+        }
+
+        public int TileHeight => tileHeight;
+
+        public List<Rectangle> GetTiles()
+        {
+            return tiles;
         }
     }
-
-
-
 }
