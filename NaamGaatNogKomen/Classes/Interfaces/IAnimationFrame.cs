86d113df0ac7cd7dfdbc5bfbda4b98035cdf610b
Haitam <Haitam.baqloul@hotmail.com>
2024-06-27 10:33:31 +0200

    small changes to the interfaces

---
+++ NaamGaatNogKomen/Classes/Interfaces/IAnimationFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaamGaatNogKomen.Classes.Interfaces
+{
+    public interface IAnimationFrame
+    {
+        double Duration { get; }
+        object Frame { get; }
+    }
+}
---
+++ NaamGaatNogKomen/Classes/Interfaces/IGraphicsSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaamGaatNogKomen.Classes.Interfaces
+{
+    public interface IGraphicsSettings
+    {
+        int Width { get; }
+        int Height { get; }
+        bool FullScreen { get; }
+    }
+}
